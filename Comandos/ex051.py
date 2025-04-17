b463fb0e65563584b87b95bc8ea0c92553feca7c
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-04-17 15:28:16 -0300

    correcao

--- Comandos/ex051.py
+++ Comandos/ex051.py
@@ -2,5 +2,5 @@ primeiro = int(input('Primeiro termo: '))
 razao = int(input('Razão: '))
 print(primeiro,' ->', end = " ")
 for c in range(1, 10):
-    print(razao* c, '->', end = " ")
+    print(primeiro + (razao * c), '->', end = " ")
 print('ACABOU!!!')
