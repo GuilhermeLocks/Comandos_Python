f95c01c85f3fadfaf3b135a8688e4c0367fb2205
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-04-24 22:14:04 -0300

    jogo

--- Comandos/ex001.py
+++ Comandos/ex001.py
@@ -2,6 +2,7 @@ import random
 aleatorio_4 = random.randint(0, 3)
 jogadas_totais = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 jogadas_diponiveis = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+jogo = 0
 print('-'*30)
 print('         JOGO DA VELHA          ')
 print('-'*30)
@@ -32,16 +33,19 @@ linha_32,
 linha_33))
 print('-'*30)
 while True:
-    jogada = input('Qual sua jogada? ')
-    if jogada.isnumeric() == True:
-        jogada = int(jogada)
-        if jogada in jogadas_totais:
-            if jogada in jogadas_diponiveis:
-                jogada = int(jogada)
-            else:
-                print('Essa posição ja esta oculpada, tente novamente.1')
-    else:
-        print('Jogada invalida tente novamente.2')
+    while True:
+        jogada = input('Qual sua jogada? ')
+        if jogada.isnumeric() == True:
+            jogada = int(jogada)
+            if jogada in jogadas_totais:
+                if jogada in jogadas_diponiveis:
+                    jogada = int(jogada)
+                    jogo += 1
+                    break
+                else:
+                    print('Essa posição ja esta oculpada, tente novamente')
+        else:
+            print('Jogada invalida tente novamente')
     if jogada == 1:
         linha_11 = 'x'
         if 1 in jogadas_diponiveis:
@@ -75,86 +79,100 @@ while True:
         if 8 in jogadas_diponiveis:
             jogadas_diponiveis.remove(8)
     if jogada == 9:
-        linha_33 = 'x'
         if 9 in jogadas_diponiveis:
+            linha_33 = 'x'
             jogadas_diponiveis.remove(9)
-    print('-' * 30)
-    print('''
-            {}  |  {}  |  {}
-          -----------------
-            {}  |  {}  |  {}
-          -----------------
-            {}  |  {}  |  {}
-    '''.format(
-    linha_11,
-    linha_12,
-    linha_13,
-    linha_21,
-    linha_22,
-    linha_23,
-    linha_31,
-    linha_32,
-    linha_33))
-    print('-' * 30)
     jogada_79 = (7, 9)
     jogada_39 = (3, 9)
     jogada_17 = (1, 7)
+    jogada_13 = (1, 3)
+    jogada_1379 = (1, 3, 7, 9)
+    ###jogadas defensivas
+    ###
 
-
-    if jogada == 1:
-        linha_33 = 'o'
+    if jogada == 1 and jogo == 1:
         if 9 in jogadas_diponiveis:
+            linha_33 = 'o'
             jogadas_diponiveis.remove(9)
 
-    if jogada == 2:
-        aleatorio_2 = random.randint(0, 1)
-        if jogada_79[aleatorio_2] in jogadas_diponiveis:
-            jogadas_diponiveis.remove(jogada_79[aleatorio_2])
-            print(jogada_79[aleatorio_2])
-            if jogada_79[aleatorio_2] == 7:
-                linha_31 = 'o'
-            if jogada_79[aleatorio_2] == 9:
-                linha_33 = 'o'
+    if linha_31 == 'x' and jogo == 2:
+        linha_21 = 'o'
+        jogadas_diponiveis.remove(4)
+    elif linha_13 == 'x' and jogo == 2:
+        linha_12 = 'o'
+        jogadas_diponiveis.remove(2)
 
-    if jogada == 3:
-        linha_31 = 'o'
-        if 7 in jogadas_diponiveis:
-            jogadas_diponiveis.remove(7)
+    if linha_31 == 'x' and jogo == 3:
+        linha_21 = 'o'
+        jogadas_diponiveis.remove(4)
+    elif linha_13 == 'x' and jogo == 2:
+        linha_12 = 'o'
+        jogadas_diponiveis.remove(2)
 
-    if jogada == 4:
-        aleatorio_2 = random.randint(0, 1)
-        if jogada_39[aleatorio_2] in jogadas_diponiveis:
-            jogadas_diponiveis.remove(jogada_39[aleatorio_2])
-            print(jogada_39[aleatorio_2])
-            if jogada_39[aleatorio_2] == 3:
-                linha_13 = 'o'
-            if jogada_79[aleatorio_2] == 9:
-                linha_33 = 'o'
+    if linha_11 == 'x' and linha_33 == 'o' and jogo == 30:
 
-    #if jogada == 5:
-        #1, 3, 7, 9
-
-    if jogada == 6:
-        aleatorio_2 = random.randint(0, 1)
-        if jogada_17[aleatorio_2] in jogadas_diponiveis:
-            jogadas_diponiveis.remove(jogada_17[aleatorio_2])
-            if jogada_17[aleatorio_2] == 1:
-                linha_11 = 'o'
-            if jogada_17[aleatorio_2] == 7:
-                linha_31 = 'o'
-
-    if jogada == 7:
-        linha_13 = 'o'
-        if 3 in jogadas_diponiveis:
+        if linha_13 == '3':
+            linha_13 = 'o'
             jogadas_diponiveis.remove(3)
+        elif linha_31 == '7':
+            linha_31 = 'o'
+            jogadas_diponiveis.remove(7)
+        elif linha_21 == '5':
+            linha_21 = 'o'
+            jogadas_diponiveis.remove(5)
+        elif linha_23 == '6':
+            linha_23 = 'o'
+            jogadas_diponiveis.remove(6)
+        elif linha_32 == '8':
+            linha_32 = 'o'
+            jogadas_diponiveis.remove(8)
 
-    #if jogada == 8:
-        # 1, 3
+    if linha_11 == 'x' and linha_33 == 'o' and jogo == 40:
 
-    if jogada == 9:
-        linha_11 = 'o'
-        if 1 in jogadas_diponiveis:
-            jogadas_diponiveis.remove(1)
+        if linha_21 == '5':
+            linha_21 = 'o'
+            jogadas_diponiveis.remove(5)
+        elif linha_23 == '6':
+            linha_23 = 'o'
+            jogadas_diponiveis.remove(6)
+        elif linha_32 == '8':
+            linha_32 = 'o'
+            jogadas_diponiveis.remove(8)
+
+    if linha_11 == 'x' and linha_33 == 'o' and jogo == 50:
+
+        if linha_21 == '5':
+            linha_21 = 'o'
+            jogadas_diponiveis.remove(5)
+        elif linha_23 == '6':
+            linha_23 = 'o'
+            jogadas_diponiveis.remove(6)
+        elif linha_32 == '8':
+            linha_32 = 'o'
+            jogadas_diponiveis.remove(8)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     print('-' * 30)
     print('''
