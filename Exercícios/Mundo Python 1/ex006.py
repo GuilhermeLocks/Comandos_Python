0e64cd34b25fd75c24244ff74a33380b3745beef
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-04-29 19:31:02 -0300

    ex

--- Exercícios/Mundo Python 1/ex006.py
+++ Exercícios/Mundo Python 1/ex006.py
@@ -1,5 +1,4 @@
-n1 = int(input('Digite um numero:'))
+n1 = int(input('Digite um numero: '))
 print('O dobro de {} vale {}.'.format(n1, (n1*2)))
-print('O triplo de {} vale {}.'.format(n1, (n1*3)))
-print('A rais quadra de {} vale {}.'.format(n1, (pow(n1, (1/2)))))
+print('O triplo de {} vale {}. \nA rais quadra de {} é igual a  {}.'.format(n1, (n1*3), n1, (pow(n1, (1/2)))))
 
