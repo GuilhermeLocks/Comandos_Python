413d3efb9db3bba6e1f3e33dc52c28032ffc707a
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-05-05 17:21:40 -0300

    ex080

--- Mundo Python 3/Exercícios/ex080.py
+++ Mundo Python 3/Exercícios/ex080.py
@@ -1,11 +1,22 @@
 valor = []
 for c in range(0, 5):
     numero = int(input('Digite um valor: '))
-    valor.append(numero)
-    valor.sort()
-    if valor[len(valor)-1] == numero:
+
+    if valor == []:
+        valor.append(numero)
         print('Adicionado ao final da lista...')
-    else:
-        print('Adicionado na posição {} da lista...'.format(valor.index(numero)))
+
+    elif numero > valor[c-1]:
+        valor.append(numero)
+        print('Adicionado ao final da lista...')
+
+    elif numero < valor[0]:
+        valor.insert(0, numero)
+        print('Adicionado no começo da lista...')
+
+    elif numero < valor[c-1] and numero > valor[c-2]:
+        valor.insert(c-1, numero)
+        print('Adicionado na posição {} da lista...'.format(c-1))
+
 print('-='*40)
 print('Os valores digitados em ordem foram {}'.format(valor))
