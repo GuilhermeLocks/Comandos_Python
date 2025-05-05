ee29300bbaba65b289bf669eac88249c48a99d6e
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-05-05 07:47:54 -0300

    ex013

--- Mundo Python 1/Exercícios/ex013.py
+++ Mundo Python 1/Exercícios/ex013.py
@@ -1,2 +1,8 @@
-n1= float(input('Qual é o salário do funcionário? R$'))
-print('Um duncionário que ganha R${}, com 15% de aumento, passa a receber R${:.2f}'.format(n1, (n1/100)*115))
+while True:
+    n1 = input('Qual é o salário do funcionário? R$')
+    if n1.isnumeric() == True:
+        n1 = float(n1)
+        break
+    else:
+        print('Salário invalido, tente novamente.')
+print('Um funcionário que ganha R${:.2f}, com 15% de aumento, passa a receber R${:.2f}'.format(n1, (n1/100)*115))
