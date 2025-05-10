0b38b4cf8a082c3029b98ca14bd26c75495f17da
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-05-09 22:50:13 -0300

    aula 19

---
+++ Mundo Python 3/Aulas/Aula 19.py
@@ -0,0 +1,13 @@
+dados = {}
+dados = {'nome':'pedro', 'idade':'25'}
+dados['sexo'] = 'M'
+print(dados['nome'])
+print(dados['idade'])
+print(dados['sexo'])
+del dados['idade']
+print(dados)
+print(dados.values())
+print(dados.keys())
+print(dados.items())
+for k, v in dados.items():
+    print(k, v)
