dd80908f70330b3f19320601b7d8ad3e3a1cbb68
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-05-15 20:54:35 -0300

    jogo

--- Mundo Python 3/Aulas/Aula 19.py
+++ Mundo Python 3/Aulas/Aula 19.py
@@ -1,13 +1,22 @@
 dados = {}
+dicionario = dict()
 dados = {'nome':'pedro', 'idade':'25'}
 dados['sexo'] = 'M'
 print(dados['nome'])
 print(dados['idade'])
 print(dados['sexo'])
 del dados['idade']
+print('')
 print(dados)
+dados['idade'] = 6
+print('')
 print(dados.values())
 print(dados.keys())
 print(dados.items())
+print('')
 for k, v in dados.items():
-    print(k, v) 
+    print('O {} é {},'.format(k, v), end=' ')
+print('.')
+for c in dados:
+    print('O {} é {}.'.format(c, dados[c]), end=' ')
+print('')
