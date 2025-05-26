e72c0294753834e56a81d34cd276c14ab4ad2bb2
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-05-25 22:03:22 -0300

    ex098

---
+++ Mundo Python 3/Exercícios/ex098.py
@@ -0,0 +1,20 @@
+import time
+def contador(i, f, p):
+    print('Contador de {} até {} de {} em {}'.format(i, f, p, p))
+    if p < 0:
+        f -= 1
+    for c in range(i, f, p):
+        print(c, end=' ')
+        time.sleep(0.5)
+    print()
+print('-='*20)
+contador(1, 11, 1)
+print('-='*20)
+contador(10, -1, -2)
+print('-='*20)
+print('Agora é sua vez de personalizar a contagem!')
+inicio = int(input('Início: '))
+fim = int(input('Fim: '))
+passo = int(input('Passo: '))
+contador(inicio, fim, passo)
+print('FIM!')
