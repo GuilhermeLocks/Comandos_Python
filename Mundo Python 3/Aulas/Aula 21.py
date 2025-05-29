e1ce4e334114c74b18b81b32cc49b96b6cf344c0
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-05-29 18:04:52 -0300

    aula 21

--- Mundo Python 3/Aulas/Aula 21.py
+++ Mundo Python 3/Aulas/Aula 21.py
@@ -1,7 +1,8 @@
 #help(dict)
 #print('-='*50)
 #print(input.__doc__)
-def contador(i, f, p):
+n = 1
+def contador(i=0, f=20, p=2):
     '''
     utilizado para realizar uma contagem com números negativos e positivos
     e podendo alterar o passo
@@ -16,8 +17,13 @@ def contador(i, f, p):
         f -= 1
     for c in range(i, f, p):
         print(c, end=' ')
+    n = 0
+    print('n = ',n)
 contador(10, 0, -1)
 print()
 contador(0, 10, 1)
 print()
+contador()
+print()
 help(contador)
+print('n = ',n)
