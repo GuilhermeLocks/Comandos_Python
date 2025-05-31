e0dc42768f0b3ffd74d7cbdd0d9f5e29dce1ca9b
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-05-31 20:20:16 -0300

    ex105

--- Mundo Python 3/Exercícios/ex105.py
+++ Mundo Python 3/Exercícios/ex105.py
@@ -1,6 +1,6 @@
 def situacao(* num, sit=False):
     maior = menor = total = cont = media = 0
-    dicionario = {'maior':0, 'menor':0}
+    dicionario = {'total':0, 'maior':0, 'menor':0}
     for c in num:
         total += c
         cont += 1
@@ -14,9 +14,13 @@ def situacao(* num, sit=False):
             dicionario['menor'] = c
     dicionario['total'] = cont
     dicionario['media'] = total/cont
+    if sit==True:
+        if 10>= dicionario['media'] >= 7:
+            dicionario['situação']='BOA'
+        elif 0 <= dicionario['media'] < 7:
+            dicionario['situação']='RUIM'
+        else:
+            dicionario['situação']='IMVALIDA'
+
     print(dicionario)
-
-
-
-
-situacao(10, 10, 10, sit=True)
+situacao(10, 10, 10, sit=False)
