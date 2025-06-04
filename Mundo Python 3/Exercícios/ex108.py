2513114e10887a808cd719755ab34a592ea4f6a0
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-06-04 10:25:37 -0300

    ex108

---
+++ Mundo Python 3/Exercícios/ex108.py
@@ -0,0 +1,7 @@
+import ex108_moeda
+
+p = float(input('Digite o preço: R$'))
+print('A metade de R${:.2f} é R${}'.format(p, ex108_moeda.metade(p)))
+print('O dobro de R${:.2f} é R${}'.format(p, ex108_moeda.dobro(p)))
+print('Aumentando 10%, temos R${}'.format(ex108_moeda.aumentar(p)))
+print('Reduzindo 13%, temos R${}'.format(ex108_moeda.reduzir(p)))
---
+++ Mundo Python 3/Exercícios/ex108_moeda.py
@@ -0,0 +1,12 @@
+def metade(a):
+    a = '{:.2f}'.format(a/2)
+    return a
+def dobro(a):
+    a ='{:.2f}'.format(a*2)
+    return a
+def aumentar(a):
+    a = '{:.2f}'.format((a/100)*110)
+    return a
+def reduzir(a):
+    a = '{:.2f}'.format((a/100)*87)
+    return a
