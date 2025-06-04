e52f1951f56508533f0e3d225395797df8648033
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-06-04 10:03:01 -0300

    ex106

--- Mundo Python 3/Exercícios/ex106.py
+++ Mundo Python 3/Exercícios/ex106.py
@@ -1,8 +1,20 @@
-print('\033[32m-='*17)
-print('     SISTEMA DE AJUDA PyHELP')
-print('-='*17)
+
+fim = 'False'
 def ajuda(a):
-    print('\033[35m')
-    help(a)
-    print('\033[m')
-ajuda(input('\033[m\033[34mFunção ou Biblioteca > \033[m'))
+    if a == 'fim':
+        print('\033[31m-='*10)
+        print('      ATE LOGO!')
+        print('-='*10, '\033[m')
+        return 'True'
+    else:
+        print('\033[35m')
+        help(a)
+        print('\033[m')
+        return 'False'
+
+
+while fim == 'False':
+    print('\033[32m-='*17)
+    print('     SISTEMA DE AJUDA PyHELP')
+    print('-=' * 17)
+    fim = ajuda(input('\033[m\033[34mFunção ou Biblioteca > \033[m'))
