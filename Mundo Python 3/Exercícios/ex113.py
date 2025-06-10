0e9aa5aa6b96588a6a73912c0f7a4a057ea72f11
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-06-10 10:01:00 -0300

    ex113

--- Mundo Python 3/Exercícios/ex113.py
+++ Mundo Python 3/Exercícios/ex113.py
@@ -1,17 +1,23 @@
 while True:
+    while True:
+        try:
+            inteiro = int(input('Digite um número inteiro: '))
+        except UnicodeDecodeError:
+            print('\nO usuário preferido não digitar o valor. ')
+            break
+        except:
+            print('\033[31mERRO! por favor, digite um valor inteiro válido.\033[m')
+        else:
+            break
     try:
-        Numero_Inteiro = int(input('Digite um número inteiro: '))
+        real = float( input('Digite um número com vírgula:'))
     except UnicodeDecodeError:
-        print('\nO usuário preferido não digitar o valor. ')
+        print('\n\033[31mO usuário preferido não digitar o valor. \033[m')
+        real = 0
+        print('O valo inteiro digitado foi {} e o real foi {}'.format(inteiro, real))
         break
     except:
-        print('ERRO! Digite um valor válido.')
-    try:
-        Numero_Quebrado = float( input('Digite um número com vírgula:'))
-    except UnicodeDecodeError:
-        print('\nO usuário preferido não digitar o valor. ')
-        break
-    except:
-        print('ERRO! Digite um valor válido.')
+        print('\033[31mERRO! por favor, digite um valor inteiro válido.\033[m')
     else:
+        print('O valo inteiro digitado foi {} e o real foi {}'.format(inteiro, real))
         break
