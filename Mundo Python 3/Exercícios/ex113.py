f02d6f74e892e02138bf085b8ee04885095a396c
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-06-10 08:39:21 -0300

    ex113

--- Mundo Python 3/Exercícios/ex113.py
+++ Mundo Python 3/Exercícios/ex113.py
@@ -1,18 +1,16 @@
 while True:
     try:
         Numero_Inteiro = int(input('Digite um número inteiro: '))
-    except KeyboardInterrupt:
-        print('O usuário preferido não digitar o valor. ')
+    except UnicodeDecodeError:
+        print('\nO usuário preferido não digitar o valor. ')
         break
     except:
         print('ERRO! Digite um valor válido.')
-    else:
-        break
-while True:
     try:
         Numero_Quebrado = float( input('Digite um número com vírgula:'))
-    except KeyboardInterrupt:
-        print('O usuário preferido não digitar o valor. ')
+    except UnicodeDecodeError:
+        print('\nO usuário preferido não digitar o valor. ')
+        break
     except:
         print('ERRO! Digite um valor válido.')
     else:
