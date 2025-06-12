ed5fa7aa50197e0dd9b6302f6d721e85c24e2604
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-06-11 22:38:23 -0300

    atu

---
+++ Mundo Python 3/Exercícios/ex115b.py
@@ -0,0 +1,42 @@
+print('-'*40)
+print('             MENU PRINCIPAL')
+print('-'*40)
+print('1 - Ver pessoas cadastradas')
+print('2 - Cadastrar nova Pessoa')
+print('3 - Sair do Sistema')
+print('-'*40)
+while True:
+    try:
+        opcao = int(input('Sua Opção: '))
+        if opcao >= 1 and opcao <=3:
+            if opcao == 1:
+                print('-' * 40)
+                print('                 Opção 1') 
+                print('-' * 40)
+                print('-' * 40)
+                print('             MENU PRINCIPAL')
+                print('-' * 40)
+                print('1 - Ver pessoas cadastradas')
+                print('2 - Cadastrar nova Pessoa')
+                print('3 - Sair do Sistema')
+                print('-' * 40)
+            if opcao == 2:
+                print('-' * 40)
+                print('                 Opção 2')
+                print('-' * 40)
+                print('-' * 40)
+                print('             MENU PRINCIPAL')
+                print('-' * 40)
+                print('1 - Ver pessoas cadastradas')
+                print('2 - Cadastrar nova Pessoa')
+                print('3 - Sair do Sistema')
+                print('-' * 40)
+            if opcao == 3:
+                print('-' * 40)
+                print('     Saindo do sistema... Ate logo!')
+                print('-' * 40)
+                break
+        else:
+            print('ERRO! Digite uma opçãp valida!')
+    except:
+        print('ERRO: por favor digite um número inteiro válido.')
