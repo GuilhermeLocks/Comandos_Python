64253a4582e5d89ea361df18911e6dc0d5dadf14
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-09-28 20:38:02 -0300

    poo alura

---
+++ Alura/OO-Sabor-Express/modelos/restaurante.py
@@ -0,0 +1,11 @@
+class restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+
+restaurante_placa = restaurante()
+restaurante_pizza = restaurante()
+
+restaurantes = [restaurante_placa, restaurante_pizza]
+
+print(restaurantes)
