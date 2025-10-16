7ed997abc422b63dfad71fe6db10c4b04e3bbe09
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-10-16 08:33:39 -0300

    poo alura

--- Alura/OO-Sabor-Express/modelos/restaurante.py
+++ Alura/OO-Sabor-Express/modelos/restaurante.py
@@ -2,25 +2,30 @@ class Restaurante:
     restarantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self.nome = nome.title()
+        self.categoria = categoria.upper()
         self._ativo = False
         Restaurante.restarantes.append(self)
 
     def __str__(self):
         return f'{self.nome} / {self.categoria}'
 
-    def listar_restauramtes():
+    @classmethod
+    def listar_restauramtes(cls):
         print(f'{'nome '.ljust(10)}{'/categoria '.ljust(15)}{'/status'.ljust(20)}')
-        for restaurante in Restaurante.restarantes:
+        for restaurante in cls.restarantes:
             print(f'{restaurante.nome.ljust(10)}/{restaurante.categoria.ljust(15)}/{restaurante.ativo.ljust(20)}')
 
     @property
     def ativo(self):
         return 'verdadeiro' if self._ativo else 'falso'
 
-restaurante_placa = Restaurante('Praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza', 'Italiana')
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+
+restaurante_placa = Restaurante('praça', 'Gourmet')
+restaurante_pizza = Restaurante('pizza', 'Italiana')
+restaurante_placa._ativo = True
 
 restaurantes = [restaurante_placa, restaurante_pizza]
 
@@ -35,7 +40,7 @@ for c in restaurantes:
 print()
 Restaurante.listar_restauramtes()
 
-
+restaurante_pizza.alternar_estado()
 
 print()
 Restaurante.listar_restauramtes()
