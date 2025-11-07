2c6975f7fa67599e6b1f8a35c6dea024cecb7c10
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-11-07 06:32:43 -0300

    POO alura

--- Alura/Sabor_Express/app.py
+++ Alura/Sabor_Express/app.py
@@ -1,10 +1,14 @@
 from modelos.restaurante import Restaurante
+from Alura.Sabor_Express.modelos.cardapio.bebida import Bebida
+from Alura.Sabor_Express.modelos.cardapio.prato import Prato
 
 restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
-restaurante_mexicano.receber_avaliacao('Gui', 10)
+bebido_suco = Bebida('Suco de melancia', 5.0, 'grande')
+prato_paozinho = Prato('Paozinho', 2.00, 'O melhor pão da cidade')
 
 def main():
-    Restaurante.listar_restauramtes()
+    print(bebido_suco)
+    print(prato_paozinho)
 
 if __name__ == '__main__':
     main()
