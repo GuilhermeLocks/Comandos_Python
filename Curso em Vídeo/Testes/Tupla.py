c503162cca849c5f91da086bad7c01f559445841
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-11-19 19:13:02 -0300

    tupla

--- Curso em Vídeo/Testes/Tupla.py
+++ Curso em Vídeo/Testes/Tupla.py
@@ -1,4 +1,5 @@
 lanche = ('hamburguer', 'batata frita', 'pudim', 'pizza')
+lanche_lista = []
 print(lanche)
 print(sorted(lanche))
 print(sorted(lanche)[1])
@@ -6,4 +7,11 @@ print()
 for c in sorted(lanche):
     if c =='hamburguer':
         c = 'comida'
+    print(c)
+print()
+for c in lanche:
+    print(c)
+    lanche_lista.append(c)
+print()
+for c in lanche_lista:
     print(c)
