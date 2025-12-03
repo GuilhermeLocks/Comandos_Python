4ba1b8e95757b5d4cbddf9363380049884162135
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-12-03 19:19:55 -0300

    mysql drop

--- Curso em Vídeo/MySQL/Criando tabela.py
+++ Curso em Vídeo/MySQL/Criando tabela.py
@@ -5,7 +5,7 @@ cursor.execute('use naruto')
 
 cursor.execute("""
 CREATE TABLE funcionarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100),
     cargo VARCHAR(100),
     salario FLOAT
---
+++ Curso em Vídeo/MySQL/excluindo tebela.py
@@ -0,0 +1,8 @@
+import mysql.connector
+con = mysql.connector.connect(host='localhost', user='root', password='')
+cursor = con.cursor()
+try:
+    cursor.execute("drop database naruto")
+    print("Naruto excluido com sucesso!")
+except:
+    print("Erro ao deletar naruto")
