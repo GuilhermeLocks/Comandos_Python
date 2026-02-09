1ba763e2e4ef39c5f8ff1cd9771c3fa8a107372b
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2026-02-09 15:34:22 -0300

    Curso Python POO: Desafio 16

--- Curso em Vídeo/Mundo Python 4/ex116.py
+++ Curso em Vídeo/Mundo Python 4/ex116.py
@@ -1,5 +1,6 @@
 from rich import print
-class funcionarioa:
+
+class Funcionario:
 
     def __init__(self, nome, setor, cargo):
         self.nome = nome
@@ -10,8 +11,9 @@ class funcionarioa:
     def apresentar(self):
         return f':handshake: Olá, sou [blue]{self.nome}[/] e sou {self.cargo} do setor de {self.setor} da empresa {self.empresa}'
 
-c1 = funcionarioa('Maria', 'Administração', 'Diretora')
+c1 = Funcionario('Maria', 'Administração', 'Diretora')
+c1.empresa = 'Curso'
 print(c1.apresentar())
 
-c1 = funcionarioa('Pedro', 'TI', 'Programador')
+c1 = Funcionario('Pedro', 'TI', 'Programador')
 print(c1.apresentar())
