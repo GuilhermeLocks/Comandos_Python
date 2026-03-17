8b46f713d34db3d7e2abb6579583b2a184476e1e
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2026-03-17 10:36:19 -0300

    Curso Python POO: Desafio 20

--- Curso em Vídeo/Mundo Python 4/ex120.py
+++ Curso em Vídeo/Mundo Python 4/ex120.py
@@ -11,16 +11,22 @@ class Gamer:
 
     def add_favoritos(self, jogo):
         lista_de_jogos.append(jogo)
+        lista_de_jogos.sort()
 
     def ficha(self):
-        lista_de_jogos.sort()
-        produto = Panel(f'Nome real: {self.nome}'
-                        f'\nJogos favoritos:'
-                        f'\n{lista_de_jogos}', title=f'Jogador <{self.nick}>')
+
+        conteudo = f'Nome real: [black on blue]{self.nome}[/]'
+        conteudo += f'\nJogos favoritos:'
+        for c in lista_de_jogos:
+            conteudo += f'\n:video_game: [blue]{c}[/]'
+
+        produto = Panel(f'{conteudo}', title=f'Jogador <{self.nick}>', width=40)
+
         print(produto)
 
-j1 = Gamer('Naruto', 'Nana')
-j1.add_favoritos('c')
-j1.add_favoritos('b')
-j1.add_favoritos('a')
+j1 = Gamer('Naruto', 'detonator2025')
+j1.add_favoritos('Mario Bros.')
+j1.add_favoritos('Sonic')
+j1.add_favoritos('God of war')
+j1.add_favoritos('Fortnite')
 j1.ficha()
