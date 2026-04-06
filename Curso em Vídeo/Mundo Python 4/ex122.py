786c1b893336535bfc58c17c4a05ae56a453cc11
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2026-04-06 17:13:54 -0300

    Controle Remoto em Python - Curso Python POO: Desafio 22

--- Curso em Vídeo/Mundo Python 4/ex122.py
+++ Curso em Vídeo/Mundo Python 4/ex122.py
@@ -1,27 +1,82 @@
 from rich.panel import Panel
 from rich import print
-class televisao:
-    def __init__(self):
 
-        print('')
-        produto = Panel(f'[red]A TV está desligada', title=f'[ TV ]', width=30, height=3)
-        print(produto)
+class ControleRemoto:
+    canal_min:int = 1
+    canal_max:int = 5
+    volume_min:int = 1
+    volume_max:int = 6
 
-        self.comando = input('< CH1 >   - VOL2 + ')
+    def __init__(self, canal = 1, volume = 2):
+        self.canal_atual:int = canal
+        self.volume_atual:int = volume
+        self.ligado:bool = False
 
-        if self.comando == '@':
-            while True:
-                print('')
-                produto = Panel(f'CANAL  = [yellow]1[/] 2 3 4 5'
-                                f'\nVOLUME = [green]1[/] 2 3 4 5', title=f'[ TV ]', width=30, height=4)
-                print(produto)
-                self.comando = input('< CH1 >   - VOL2 + ')
+    def mostrar_tv(self):
+        conteudo = ''
+        if not self.ligado:
+            conteudo = f':prohibited: [red] A TV está desligada'
+        else:
+            conteudo = f'CANAL  ='
+            for canal in range(ControleRemoto.canal_min, ControleRemoto.canal_max + 1):
+                if canal == self.canal_atual:
+                    conteudo += f' [yellow on yellow] {canal} [/]'
+                else:
+                    conteudo += f' {canal} '
 
-                if self.comando == '@':
-                    break
+            conteudo += '\nVOLUME = '
+            for volume in range(ControleRemoto.volume_min, ControleRemoto.volume_max + 1):
+                if volume <= self.volume_atual:
+                    conteudo += f'[black on cyan]  [/]'
+                else:
+                    conteudo += f'[black on white]  [/]'
 
-        print('')
-        produto = Panel(f'[red]A TV está desligada', title=f'[ TV ]', width=30, height=3)
-        print(produto)
+        tv = Panel(conteudo, title=f'[ TV ]',width=30)
+        print(tv)
 
-t1 = televisao()
+    def liga_desliga(self):
+        self.ligado = not self.ligado
+
+    def canal_mais(self):
+        if self.ligado:
+            if self.canal_atual == ControleRemoto.canal_max:
+                self.canal_atual = ControleRemoto.canal_min
+            else:
+                self.canal_atual += 1
+
+    def canal_menos(self):
+        if self.ligado:
+            if self.canal_atual == ControleRemoto.canal_min:
+                self.canal_atual = ControleRemoto.canal_max
+            else:
+                self.canal_atual -= 1
+
+    def volume_mais(self):
+        if self.ligado:
+            if self.volume_atual != ControleRemoto.volume_max:
+                self.volume_atual += 1
+
+
+    def volume_menos(self):
+        if self.ligado:
+            if self.volume_atual != ControleRemoto.volume_min:
+                self.volume_atual -= 1
+
+
+c = ControleRemoto()
+while True:
+    c.mostrar_tv()
+    comando = str(input(f'\n < CH{c.canal_atual} > - VOL{c.volume_atual} + '))
+    match comando:
+        case '0':
+            break
+        case '@':
+            c.liga_desliga()
+        case '>':
+            c.canal_mais()
+        case '<':
+            c.canal_menos()
+        case '-':
+            c.volume_menos()
+        case '+':
+            c.volume_mais()
