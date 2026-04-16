d8218ad6078d534a0ad8b34cb9a3688088266298
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2026-04-16 17:47:52 -0300

    TEST

--- ChatGPT/Herança.py
+++ ChatGPT/Herança.py
@@ -6,8 +6,13 @@ class Animal:
         print('Som genérico de animal')
 
 class Cachorro(Animal):
+
+    def __init__(self, nome):
+        super().__init__(nome)
+        self.nome = nome
+
     def falar(self):
-        print('Au Au!')
+        print(f'{self.nome} falou Au Au!')
 
 class Gato(Animal):
     def falar(self):
